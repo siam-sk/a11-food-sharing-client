db91c214f66df86af47f50b59c560004b4dd69ff
Siam Sheikh <siammsheikh09@gmail.com>
2025-06-17 22:58:19 +0600

    minor fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -83,6 +83,7 @@ const Login = () => {
   return (
     <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8"> 
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <UserCircleIcon className="mx-auto h-16 w-auto text-sky-600" />
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Sign in to your account
         </h2>
